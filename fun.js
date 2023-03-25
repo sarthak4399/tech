54a22b5722493db44d9a5f62a272b61351255d55
sarthak4399 <sarthakkhandare21@gmail.com>
2023-03-25 17:28:15 +0530

    websocket

--- fun.js
+++ fun.js
@@ -26,5 +26,42 @@ for (let i = 0; i < key.length; i++) {
         else if (key[i] == '0' && i == 3) {
             document.getElementById('q3').style.backgroundColor = "white";
         }
+        
 }
+let socket;
+let reconnects = 0;
+function connectWebsocket(url) {
+	socket = new WebSocket(url);
+	// socket.onmessage = function onmessage_callback(_message) {
+	// 	console.log("sdkfjslkdjf")
+	//
+	// };
+	//
+	socket.onmessage = function(s) {
+		console.log(s)
+	};
+	socket.onopen = function onopen_callback() {
+		console.log("Connected to: " + socket.url);
+		// socket.send(JSON.stringify({ "Hi": "from client" }));
+	};
+	socket.onclose = function onclose_callback(_event) {
+		console.error("Connection Closed: ");
+		reconnects++;
+		if (reconnects > 10) {
+			console.error("Tried reconnecting {reconnect} times");
+		} else {
+			console.warn("Trying to reconnect");
+			setTimeout(function() {
+				connectWebsocket(url);
+			}, 5000);
+		}
+	};
+	socket.onerror = function onerror_callback(_) {
+		console.warn("Error Occured. Trying to reconnect.");
+		setTimeout(function() {
+			connectWebsocket(url);
+		}, 5000);
+	};
+}
+connectWebsocket("ws://localhost:5500");
     
